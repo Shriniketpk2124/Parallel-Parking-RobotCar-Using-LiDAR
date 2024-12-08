935d0c3347b5582b61785ccf14e949257df450d9
Shriniketpk2124 <Shriniketpk2124@gmail.com>
2024-12-08 13:23:02 +0100

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to check if the previous script executed successfully
 check_exit_status() {
     if [ $? -ne 0 ]; then
         echo "Error: $1 did not terminate properly. Exiting..."
@@ -8,27 +7,22 @@ check_exit_status() {
     fi
 }
 
-# Step 1: Run initial_wall_following.py
 echo "Running shrinket_test.py..."
 python3 "initial_wall_following.py"
 check_exit_status "initial_wall_following.py"
 
-# Step 2: Run turn_to_park.py
 echo "Running turn_to_park.py..."
 python3 "turn_to_park.py"
 check_exit_status "turn_to_park.py"
 
-# Step 3: Run reverse_into_parking_space.py
 echo "Running reverse_into_parking_space.py..."
 python3 "reverse_into_parking_space.py"
 check_exit_status "reverse_into_parking_space.py"
 
-# Step 4: Run final_parking_turn.py
 echo "Running aligning_test_shriniket.py..."
 python3 "final_parking_turn.py"
 check_exit_status "final_parking_turn.py"
 
-# Step 5: Run check_alignment.py
 echo "Running trial_aligning.py..."
 python3 "check_alignment.py"
 check_exit_status "check_alignment.py"
